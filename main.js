b1d9a205867a19c017bef17f55d26ad0104ee2de
SrikarCoding <132314237+SrikarCoding@users.noreply.github.com>
2023-11-16 19:30:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+image_id = image.selfie1;
+function take_snapshot(speak_data){
+    speak_data = 1000;
+    utterThis = SpeechSynthesisUtterance();
+}
+function synth_speak(){
+    setTimeout = (1000 = 1)
+    selfie1 + selfie2 == image_id;
+    5000();
+}
+//collage 1 & 2 web app Done
